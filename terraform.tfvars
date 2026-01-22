8b4e7144d2f6cb8556b6ca5d4ff4d50a78b89f91
Arezou Montazeri <arezoo.montazeri69@gmail.com>
2026-01-22 19:01:55 +0100

    create EC2
    define variables

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+ami_id        = "ami-0683ee28af6610487"
+instance_type = "t3.micro"
+key_name      = "key1"
+machine_name  = "machine_name"
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "ami_id" {
+  type        = string
+  description = "this is ami_id for the machine"
+}
+variable "instance_type" {}
+variable "key_name" {}
+variable "machine_name" {}
