9201d1399227bfa37e82b4b5e5ed761d5e402e7f
Arezou Montazeri <arezoo.montazeri69@gmail.com>
2026-02-16 08:59:16 +0100

    Add dynamic subnets, route tables, and association

---
+++ project1&2/terraform-aws-lab/security.tf
@@ -0,0 +1,50 @@
+resource "aws_security_group" "main_sg" {
+  name        = "main-sg"
+  description = "Allow HTTP, HTTPS, SSH, ICMP"
+  vpc_id      = aws_vpc.main.id
+
+  # Inbound rules
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "ICMP (ping)"
+    from_port        = -1
+    to_port          = -1
+    protocol         = "icmp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  # Outbound (allow all by default)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "main-sg"
+  }
+}
