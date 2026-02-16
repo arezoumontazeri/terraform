407a35faf170eed80bcd9c6db0f91d1c7007d510
hamed.enayatzare@gmail.com <arezoo.montazeri69@gmail.com>
2026-02-16 08:59:16 +0100

    Add dynamic subnets, route tables, and association

--- project1&2/terraform-aws-lab/routs.tf
+++ project1&2/terraform-aws-lab/routs.tf
@@ -6,21 +6,36 @@ resource "aws_route_table" "route_table" {
   }
 }
 resource "aws_route" "route_internet" {
-  for_each               = var.route_tables
+  #for_each               = var.route_tables
+  for_each = {
+    for k, v in var.route_tables : k => v
+    if v.is_public
+  }
+
   route_table_id         = aws_route_table.route_table[each.key].id
   gateway_id             = aws_internet_gateway.igw.id
-  destination_cidr_block = var.route_tables[each.key].is_public ? "0.0.0.0/0" : null
-
-}
-resource "aws_route_table_association" "private_assoc" {
-  subnet_id      = aws_subnet.sub["private_subnet"].id
-  route_table_id = aws_route_table.route_table["private"].id
+  destination_cidr_block = "0.0.0.0/0"
+    #route_table_id         = aws_route_table.route_table[each.key].id
+  #gateway_id             = aws_internet_gateway.igw.id
+  #destination_cidr_block = var.route_tables[each.key].is_public ? "0.0.0.0/0" : null
 }
 
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.sub["public_subnet"].id
-  route_table_id = aws_route_table.route_table["public"].id
+resource "aws_route_table_association" "assoc" {
+  for_each = var.subnets
+
+  subnet_id      = aws_subnet.sub[each.key].id
+  route_table_id = aws_route_table.route_table[each.key].id
 }
+
+#resource "aws_route_table_association" "private_assoc" {
+#  subnet_id      = aws_subnet.sub["private"].id
+#  route_table_id = aws_route_table.route_table["private"].id
+#}
+
+#resource "aws_route_table_association" "public_assoc" {
+#  subnet_id      = aws_subnet.sub["public"].id
+#  route_table_id = aws_route_table.route_table["public"].id
+#}
 
 
 
